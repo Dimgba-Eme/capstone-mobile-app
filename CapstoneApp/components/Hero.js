f17231a072ab7022eee424e921f93e6058d6fdf8
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-04 19:04:25 +0100

    Imported and aligned the Hero image

--- CapstoneApp/components/Hero.js
+++ CapstoneApp/components/Hero.js
@@ -4,14 +4,14 @@ export default function Hero() {
     return (
         <View style={styles.container}>
 
-            <View>
-            <Text style={styles.title}>Little Lemon</Text>
-            <Text style={styles.innerText}>Chicago</Text>
-            <Text style={styles.intro}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. </Text>
+            <View style={styles.innerContainer}>
+                <Text style={styles.title}>Little Lemon</Text>
+                <Text style={styles.innerText}>Chicago</Text>
+                <Text style={styles.introText}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. </Text>
             </View>
 
             <View>
-                <Image />
+                <Image style={styles.heroImage} source={require('../assets/Hero image.png')} resizeMode='cover' accessibility={true} accessibilityLabel='Hero Image' />
             </View>
         </View>
     );
@@ -21,6 +21,14 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#495E57',
         paddingHorizontal: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+
+    innerContainer: {
+        paddingVertical: 20,
+        width: 180,
     },
 
     title: {
@@ -35,7 +43,16 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
 
-    intro: {
+    introText: {
         color: '#fff',
-    }
+        fontSize: 15,
+        marginVertical: 20,
+    },
+
+    heroImage: {
+        height: 130,
+        width: 130,
+        marginTop: 70,
+        borderRadius: 10,
+    },
 })
