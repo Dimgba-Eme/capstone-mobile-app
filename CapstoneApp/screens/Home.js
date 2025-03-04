8582f07fed2c62d548bdc292408baf718937dec6
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-04 21:54:28 +0100

    Imported, styled and aligned the menu images in the home screen

--- CapstoneApp/screens/Home.js
+++ CapstoneApp/screens/Home.js
@@ -46,6 +46,54 @@ export default function Home() {
                     </View>
 
                 </View>
+                <View style={styles.menuItemsContainer}>
+                    <View style={styles.menuInner}>
+                        <Text style={styles.menuTitle}>Bruschetta</Text>
+                        <Text style={styles.menuDescription}>Our Bruschetta is made from grilled bread that has been smeared with garli...</Text>
+                        <Text style={styles.menuPrice}>$7.99</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../assets/Bruschetta.png')} resizeMode='cover' style={styles.menuImg} />
+                    </View>
+
+                </View>
+                <View style={styles.menuItemsContainer}>
+                    <View style={styles.menuInner}>
+                        <Text style={styles.menuTitle}>Grilled Fish</Text>
+                        <Text style={styles.menuDescription}>Barbequed catch of the day with red onion, crisp capers, chive creme fraiche...</Text>
+                        <Text style={styles.menuPrice}>$20.00</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../assets/Grilled fish.png')} resizeMode='cover' style={styles.menuImg} />
+                    </View>
+
+                </View>
+                <View style={styles.menuItemsContainer}>
+                    <View style={styles.menuInner}>
+                        <Text style={styles.menuTitle}>Pasta</Text>
+                        <Text style={styles.menuDescription}>Penne with fried aubergines, tomato sauce, fresh chilli, garlic, basil & salted...</Text>
+                        <Text style={styles.menuPrice}>$18.99</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../assets/Pasta.png')} resizeMode='cover' style={styles.menuImg} />
+                    </View>
+
+                </View>
+                <View style={styles.menuItemsContainer}>
+                    <View style={styles.menuInner}>
+                        <Text style={styles.menuTitle}>Lemon Dessert</Text>
+                        <Text style={styles.menuDescription}>Light and fluffy traditional homemade Italian Lemon and ricotta cake...</Text>
+                        <Text style={styles.menuPrice}>$6.99</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../assets/Lemon dessert.png')} resizeMode='cover' style={styles.menuImg} />
+                    </View>
+
+                </View>
             </ScrollView>
 
         </View>
@@ -74,7 +122,7 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        marginVertical: 15,
+        paddingVertical: 15,
         paddingBottom: 30,
         borderBottomWidth: 1,
         borderBottomColor: '#333333',
@@ -98,27 +146,38 @@ const styles = StyleSheet.create({
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
+        borderBottomColor: '#333333',
+        borderBottomWidth: 0.4,
+        paddingVertical: 20,
+        paddingBottom: 40,
     },
 
     menuInner: {
-        width: '70%'
+        width: '70%',
     },
 
 
     menuTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333333',
 
     },
 
     menuDescription: {
-
+        color: '#333333',
+        marginVertical: 5,
     },
 
     menuPrice: {
-
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#333333',
     },
 
     menuImg: {
-        height: 100,
-        width: 100,
+        height: 90,
+        width: 90,
+        marginTop: 30,
     },
 })
