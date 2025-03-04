9ad89d54b0c5b0ae1a8338dab542b4e01826e3ba
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-04 19:45:58 +0100

    Designed and aligned the menu buttons

--- CapstoneApp/screens/Home.js
+++ CapstoneApp/screens/Home.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 
@@ -8,14 +8,73 @@ export default function Home() {
         <View style={styles.container}>
             <Header />
             <Hero />
+            <ScrollView style={styles.innerContainer}>
+                <Text style={styles.orderText}>ORDER FOR DELIVERY!</Text>
 
-            {/* <Text>Home</Text> */}
+                <View style={styles.btnContainer} >
+                    <TouchableOpacity>
+                        <View style={styles.btn}>
+                            <Text style={styles.btnText}>Starters</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.btn}>
+                            <Text style={styles.btnText}>Mains</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.btn}>
+                            <Text style={styles.btnText}>Desserts</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.btn}>
+                            <Text style={styles.btnText}>Drinks</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
         backgroundColor: 'white',
-    }
+    },
+
+    innerContainer: {
+        paddingHorizontal: 20,
+        paddingVertical: 30,
+        backgroundColor: '#fff',
+    },
+
+    orderText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#333333',
+    },
+
+    btnContainer: {
+       display: 'flex',
+       flexDirection: 'row',
+       justifyContent: 'space-between'
+    },
+
+    btn: {
+       backgroundColor: '#ccc',
+       width: '100%',
+       padding: 10,
+       borderRadius: 10,
+    },
+
+    btnText: {
+        fontSize: 16,
+        color: '#333333',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+
 })
