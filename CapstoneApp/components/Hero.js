ffc5e7e7bfd47fa29b52ea06270999417e8c542c
Dimgba Agwu Eme <dimgbaagwueme@gmail.com>
2025-03-04 17:30:20 +0100

    Working on the Hero section component

--- CapstoneApp/components/Hero.js
+++ CapstoneApp/components/Hero.js
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function Hero() {
     return (
         <View>
-            <Text>Hero</Text>
+            <Text>Little Lemon</Text>
+            <Text>Chicago</Text>
+            <Text>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. </Text>
         </View>
     );
 }
